7c95a20c61b24d9d9113152f6281310ee6f09e80
Ray Laciste <raylaciste@yahoo.com>
2025-05-06 18:08:50 -0700

    add basic physics ball throw

--- Client/myGame/MyGame.java
+++ Client/myGame/MyGame.java
@@ -23,6 +23,13 @@ import net.java.games.input.*;
 import net.java.games.input.Component.Identifier.*;
 import tage.networking.IGameConnection.ProtocolType;
 
+// Physics
+import tage.physics.PhysicsEngine;
+import tage.physics.PhysicsObject;
+import tage.physics.JBullet.*;
+import com.bulletphysics.dynamics.RigidBody;
+import com.bulletphysics.collision.dispatch.CollisionObject;
+
 public class MyGame extends VariableFrameRateGame {
 	JumpAction jumpAction;
 
@@ -60,6 +67,19 @@ public class MyGame extends VariableFrameRateGame {
 		return npcTex;
 	}
 
+	// physics
+	private PhysicsEngine physicsEngine;
+	private boolean running = true;
+	private float vals[] = new float[16];
+	private boolean showPhysicsWorld = false;
+
+	// Throwable sphere
+	private GameObject sphere;
+	private ObjShape sphereS;
+	private TextureImage sphereTx;
+	private PhysicsObject sphereP;
+	private boolean sphereCreated = false;
+
 	private String serverAddress;
 	private int serverPort;
 	private ProtocolType serverProtocol;
@@ -96,6 +116,9 @@ public class MyGame extends VariableFrameRateGame {
 		// npc
 		npcShape = new ImportedModel("frog.obj");
 
+		// sphere (throwable)
+		sphereS = new Sphere();
+
 		linxS = new Line(new Vector3f(0f, 0f, 0f), new Vector3f(3f, 0f, 0f));
 		linyS = new Line(new Vector3f(0f, 0f, 0f), new Vector3f(0f, 3f, 0f));
 		linzS = new Line(new Vector3f(0f, 0f, 0f), new Vector3f(0f, 0f, -3f));
@@ -108,6 +131,7 @@ public class MyGame extends VariableFrameRateGame {
 		groundTx = new TextureImage("grass.png");
 		npcTex = new TextureImage("frog.png");
 		heightMapTx = new TextureImage("terrain.png");
+		sphereTx = new TextureImage("water.jpg");
 	}
 
 	@Override
@@ -178,6 +202,22 @@ public class MyGame extends VariableFrameRateGame {
 		// ----------------- initialize camera ----------------
 		positionCameraBehindAvatar();
 
+		// Initialize physics system
+		float[] gravity = { 0f, -5f, 0f };
+		physicsEngine = (engine.getSceneGraph()).getPhysicsEngine();
+		physicsEngine.setGravity(gravity);
+
+		// Create physics for the terrain
+		float up[] = { 0, 1, 0 };
+		double[] terrainTransform = toDoubleArray(terr.getLocalTranslation().get(vals));
+		PhysicsObject terrainP = (engine.getSceneGraph()).addPhysicsStaticPlane(
+				terrainTransform, up, 0.0f);
+		terrainP.setBounciness(1.0f);
+		terr.setPhysicsObject(terrainP);
+		// Enable physics world rendering for debugging
+		engine.enableGraphicsWorldRender();
+		engine.enablePhysicsWorldRender();
+
 		// ----------------- INPUTS SECTION -----------------------------
 		im = engine.getInputManager();
 
@@ -243,6 +283,37 @@ public class MyGame extends VariableFrameRateGame {
 			jumpAction.update();
 		}
 
+		if (sphereCreated) {
+			checkSphereLifetime();
+		}
+
+		// Update physics
+		if (running) {
+			AxisAngle4f aa = new AxisAngle4f();
+			Matrix4f mat = new Matrix4f();
+			Matrix4f mat2 = new Matrix4f().identity();
+			Matrix4f mat3 = new Matrix4f().identity();
+
+			checkForCollisions();
+			physicsEngine.update((float) elapsedTime);
+
+			for (GameObject go : engine.getSceneGraph().getGameObjects()) {
+				if (go.getPhysicsObject() != null) {
+					// Set translation
+					mat.set(toFloatArray(go.getPhysicsObject().getTransform()));
+					mat2.set(3, 0, mat.m30());
+					mat2.set(3, 1, mat.m31());
+					mat2.set(3, 2, mat.m32());
+					go.setLocalTranslation(mat2);
+
+					// Set rotation
+					mat.getRotation(aa);
+					mat3.rotation(aa);
+					go.setLocalRotation(mat3);
+				}
+			}
+		}
+
 		Vector3f loc = avatar.getWorldLocation();
 		float height = terr.getHeight(loc.x(), loc.z());
 		avatar.setLocalLocation(new Vector3f(loc.x(), height, loc.z()));
@@ -272,29 +343,187 @@ public class MyGame extends VariableFrameRateGame {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		switch (e.getKeyCode()) {
-			// case KeyEvent.VK_W: {
-			// Vector3f oldPosition = avatar.getWorldLocation();
-			// Vector4f fwdDirection = new Vector4f(0f, 0f, 1f, 1f);
-			// fwdDirection.mul(avatar.getWorldRotation());
-			// fwdDirection.mul(0.05f);
-			// Vector3f newPosition = oldPosition.add(fwdDirection.x(), fwdDirection.y(),
-			// fwdDirection.z());
-			// avatar.setLocalLocation(newPosition);
-			// protClient.sendMoveMessage(avatar.getWorldLocation());
-			// break;
-			// }
-			// case KeyEvent.VK_D: {
-			// Matrix4f oldRotation = new Matrix4f(avatar.getWorldRotation());
-			// Vector4f oldUp = new Vector4f(0f, 1f, 0f, 1f).mul(oldRotation);
-			// Matrix4f rotAroundAvatarUp = new Matrix4f().rotation(-.01f,
-			// new Vector3f(oldUp.x(), oldUp.y(), oldUp.z()));
-			// Matrix4f newRotation = oldRotation;
-			// newRotation.mul(rotAroundAvatarUp);
-			// avatar.setLocalRotation(newRotation);
-			// break;
-			// }
+			case KeyEvent.VK_R: {
+				// Create and throw a sphere when R is pressed
+				createThrowableSphere();
+
+				// Start physics if not already running
+				if (!running) {
+					running = true;
+					System.out.println("starting physics");
+				}
+				break;
+			}
+			case KeyEvent.VK_T: {
+				// Toggle physics world rendering
+				showPhysicsWorld = !showPhysicsWorld;
+				if (showPhysicsWorld) {
+					engine.enablePhysicsWorldRender();
+					System.out.println("showing physics world");
+				} else {
+					engine.disablePhysicsWorldRender();
+					System.out.println("hiding physics world");
+				}
+				break;
+			}
 		}
 		super.keyPressed(e);
+	}
+
+	// * ---------- Physics throw SECTION ----------------
+	private long sphereCreationTime = 0;
+	private final long SPHERE_LIFETIME = 2500; // 2.5 seconds
+
+	private void createThrowableSphere() {
+		if (sphereCreated) {
+			// If a sphere already exists, remove it from the scene graph and physics engine
+			if (sphere.getPhysicsObject() != null) {
+				int physicsObjectUID = sphere.getPhysicsObject().getUID();
+				physicsEngine.removeObject(physicsObjectUID);
+				sphere.setPhysicsObject(null);
+			}
+			if (sphere != null) {
+				engine.getSceneGraph().removeGameObject(sphere);
+			}
+			sphereCreated = false;
+		}
+
+		// Create a new sphere
+		sphere = new GameObject(GameObject.root(), sphereS, sphereTx);
+
+		// Get avatar position and direction
+		Vector3f avatarPos = avatar.getWorldLocation();
+		Vector4f fwdDirection = new Vector4f(0f, 0f, 1f, 1f);
+		fwdDirection.mul(avatar.getWorldRotation());
+		fwdDirection.normalize();
+
+		// Position the sphere at the avatar's position but slightly higher
+		Vector3f spherePos = new Vector3f(
+				avatarPos.x(),
+				avatarPos.y() + 0.25f, // Position slightly above the avatar
+				avatarPos.z());
+
+		// Set sphere properties
+		Matrix4f sphereTranslation = (new Matrix4f()).translation(spherePos);
+		sphere.setLocalTranslation(sphereTranslation);
+		Matrix4f sphereScale = (new Matrix4f()).scaling(0.07f);
+		sphere.setLocalScale(sphereScale);
+
+		// Create physics for the sphere
+		float mass = 1.0f;
+		float radius = 0.07f;
+		double[] tempTransform = toDoubleArray(sphereTranslation.get(vals));
+
+		// Add to physics engine with a unique ID
+		int newUID = physicsEngine.nextUID();
+		sphereP = physicsEngine.addSphereObject(newUID, mass, tempTransform, radius);
+
+		physicsEngine.update(16);
+
+		sphereP.setBounciness(0.5f);
+		sphere.setPhysicsObject(sphereP);
+
+		// Set up direction with upward component
+		Vector3f tossDir = new Vector3f(fwdDirection.x(), fwdDirection.y(), fwdDirection.z());
+		tossDir.add(0.0f, 0.45f, 0.0f);
+		tossDir.normalize();
+
+		// Apply force immediately for throwing
+		float tossForce = 150.0f;
+		float forceX = tossDir.x() * tossForce;
+		float forceY = tossDir.y() * tossForce;
+		float forceZ = tossDir.z() * tossForce;
+
+		// Apply force at the center of the sphere
+		sphereP.applyForce(forceX, forceY, forceZ, 0.0f, 0.0f, 0.0f);
+
+		float[] initialVelocity = { tossDir.x() * 2.5f, tossDir.y() * 2.5f, tossDir.z() * 2.5f };
+		sphereP.setLinearVelocity(initialVelocity);
+
+		// Start tracking the sphere's lifetime
+		sphereCreationTime = System.currentTimeMillis();
+		sphereCreated = true;
+
+		System.out.println("Sphere created and thrown");
+	}
+
+	// Check and remove the sphere if its lifetime has expired
+	private void checkSphereLifetime() {
+		if (sphereCreated && System.currentTimeMillis() - sphereCreationTime > SPHERE_LIFETIME) {
+			removeSphere();
+		}
+	}
+
+	// Remove the sphere from the scene and physics engine
+	private void removeSphere() {
+		if (sphereCreated) {
+			// Remove from physics engine
+			if (sphere.getPhysicsObject() != null) {
+				int physicsObjectUID = sphere.getPhysicsObject().getUID();
+				physicsEngine.removeObject(physicsObjectUID);
+				sphere.setPhysicsObject(null);
+			}
+
+			// Remove from scene graph
+			engine.getSceneGraph().removeGameObject(sphere);
+
+			sphereCreated = false;
+			sphereP = null;
+			System.out.println("Sphere removed after lifetime expired");
+		}
+	}
+
+	// ---------- PHYSICS UTILITY METHODS ----------------
+
+	private void checkForCollisions() {
+		com.bulletphysics.dynamics.DynamicsWorld dynamicsWorld;
+		com.bulletphysics.collision.broadphase.Dispatcher dispatcher;
+		com.bulletphysics.collision.narrowphase.PersistentManifold manifold;
+		com.bulletphysics.dynamics.RigidBody object1, object2;
+		com.bulletphysics.collision.narrowphase.ManifoldPoint contactPoint;
+
+		dynamicsWorld = ((JBulletPhysicsEngine) physicsEngine).getDynamicsWorld();
+		dispatcher = dynamicsWorld.getDispatcher();
+		int manifoldCount = dispatcher.getNumManifolds();
+
+		for (int i = 0; i < manifoldCount; i++) {
+			manifold = dispatcher.getManifoldByIndexInternal(i);
+			object1 = (com.bulletphysics.dynamics.RigidBody) manifold.getBody0();
+			object2 = (com.bulletphysics.dynamics.RigidBody) manifold.getBody1();
+
+			JBulletPhysicsObject obj1 = JBulletPhysicsObject.getJBulletPhysicsObject(object1);
+			JBulletPhysicsObject obj2 = JBulletPhysicsObject.getJBulletPhysicsObject(object2);
+
+			for (int j = 0; j < manifold.getNumContacts(); j++) {
+				contactPoint = manifold.getContactPoint(j);
+				if (contactPoint.getDistance() < 0.0f) {
+					System.out.println("---- hit between " + obj1 + " and " + obj2);
+					break;
+				}
+			}
+		}
+	}
+
+	private float[] toFloatArray(double[] arr) {
+		if (arr == null)
+			return null;
+		int n = arr.length;
+		float[] ret = new float[n];
+		for (int i = 0; i < n; i++) {
+			ret[i] = (float) arr[i];
+		}
+		return ret;
+	}
+
+	private double[] toDoubleArray(float[] arr) {
+		if (arr == null)
+			return null;
+		int n = arr.length;
+		double[] ret = new double[n];
+		for (int i = 0; i < n; i++) {
+			ret[i] = (double) arr[i];
+		}
+		return ret;
 	}
 
 	// ---------- NETWORKING SECTION ----------------
